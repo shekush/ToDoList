da8155df9e5c3859ab63c07af6e8814c6cca79cf
Shejal <shejal270999@gmail.com>
2021-12-10 20:13:55 +0530

    many functionalities like adding new items in our list was added

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    let today = new Date();
+    
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { day: day, items: items });
+
+});
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+    console.log("Server Started on port 3000");
+});
