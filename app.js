62d74f6b90fa6c787817b0ae9940be58d41dded9
Shejal <shejal270999@gmail.com>
2021-12-11 16:06:04 +0530

    modified about page

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ app.post("/work", function (req, res) {
     res.redirect("/work");
 });
 
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
 app.listen(3000, function () {
     console.log("Server Started on port 3000");
 });
